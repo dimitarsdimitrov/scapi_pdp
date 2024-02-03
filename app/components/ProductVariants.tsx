910b07fb83e4bd9e208fb7551f6e731c802746aa
dimitarsdimitrov <123095191+dimitarsdimitrov@users.noreply.github.com>
2024-02-03 14:56:57 +0200

    ok111

--- app/components/ProductVariants.tsx
+++ app/components/ProductVariants.tsx
@@ -1,30 +1,31 @@
 "use client";
 import React from 'react'
-import { redirect } from 'next/navigation';
-import { useRouter } from "next/router";
+import { useRouter } from 'next/navigation'
 
-export const ProductVariants = ({variants }) => {
-
+export const ProductVariants = ({variants}) => {
     const router = useRouter();
 
-    function onClickProductSize(size) {
+    const handleClick = (e) => {
+        e.preventDefault();
+        var size = e.target.dataset.value;
         const variantItem = variants.filter(function(v) { return size === v.variationValues.size });
         if (variantItem) {
             var productId = variantItem[0].productId;
-            document.location.href = 'product?pid=' + productId;
-            router.push( 'product?pid=' + productId);
+            const selectedProduct = '/product?pid=' + productId;
+            router.push(selectedProduct);
         }
 
         return false;
     }
+
 
   return (
         <div className="space-x-2 flex text-sm">
             {variants && variants.map(variant => 
                 <label key={variant}>
                     <input className="sr-only peer" name="size" type="radio" value={variant.variationValues.size} />
-                    <button
-                        onClick={() => onClickProductSize(variant.variationValues.size) }
+                    <button  data-value={variant.variationValues.size} 
+                        onClick={handleClick}
                         className="h-10 px-6 font-semibold rounded-md border border-slate-200 text-slate-900">{variant.variationValues.size}
                     </button>
             </label>
